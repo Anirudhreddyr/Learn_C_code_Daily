e328f9495dcd9440063bca78e559e1945ecbe388
Anirudh Reddy R <anirudhreddy1710@gmail.com>
2025-03-09 14:21:56 +0530

    Insertionsort using C

---
+++ 9Mar25/Insertion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void insertionsort(int arr[], int n){
+    for(int i = 1; i < n; i++){
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
